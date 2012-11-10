d362d9bba742d073ee241197d723709a12c27ec8
Patrick Flor <pflor@ku.edu>
2012-11-10 11:51:12 -0600

    Learning flask url handling

--- pe.py
+++ pe.py
@@ -7,7 +7,15 @@ app = Flask(__name__)
 def hello():
         return 'It\'s Parallelevent!!'
 
+@app.route('/foo/')
+def fello():
+        return 'Foo Parallelevent!!'
+
+@app.route('/bar/')
+def bello():
+        return 'Bar Parallelevent!!'
+    
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
