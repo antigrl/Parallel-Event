21baaf195e0ff8bb6cdb8c4c5df09f640e21712c
Kim Walta <kimwalta@gmail.com>
2012-11-10 16:19:49 -0600

    adding login drop down

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function() {
-	
+$(document).ready(function(){
+        $('#login-trigger').click(function(){
+                $(this).next('#login-content').slideToggle();
+                $(this).toggleClass('active');                                  
+
+                if ($(this).hasClass('active')) $(this).find('span').html('▲')
+                        else $(this).find('span').html('▼')
+                })
 });
