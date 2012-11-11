424235a89ce20cef83005e851eb55a9c545826a6
chris-hudson <chris.hudson@ku.edu>
2012-11-10 19:39:45 -0600

    get calendar events

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 import simplejson
 
+import gdata.calendar.service
+import cal_events
 
 def index(request, template='index.html'):
     services = [
@@ -16,3 +20,21 @@ def index(request, template='index.html'):
             template, locals(), context_instance=RequestContext(request)
         )
     return response
+
+def GetAuthSubUrl():
+    #next = 'http://www.parallelevent.com/gauthcomplete'
+    next = 'http://localhost:8000/gauthcomplete'
+    scope = 'http://www.google.com/calendar/feeds/'
+    secure = False
+    session = True
+    calendar_service = gdata.calendar.service.CalendarService()
+    return calendar_service.GenerateAuthSubURL(next, scope, secure, session)
+
+def gauth_redirect(request):
+    url = GetAuthSubUrl()
+    return HttpResponseRedirect(str(url))
+
+def gauth_complete(request):
+    token = request.GET.get('token')
+    events, _ = cal_events.getAllEvents(token)
+    return HttpResponse(str(events))
